4be21443b3aa48ce1e013d60495fd753a2bce4e0
Rafida <zakariarafida81@gmail.com>
2024-06-10 09:43:12 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn-btn')
+btn.onclick = function(){
+    alert('button has been clicked')
+}
+
+const better = document.getElementById('btn-better')
+better.onclick = function(){
+    alert('watch video')
+}
